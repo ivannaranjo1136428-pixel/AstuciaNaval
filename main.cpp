891bb8d5977b1d0128d98e7c5eff966a75bef80f
JohanGaleano <johangaleano1139853@correo.itm.edu.co>
2025-09-23 22:34:07 -0500

    creacion clase barco

--- main.cpp
+++ main.cpp
@@ -9,3 +9,19 @@
 
 using namespace std;
 
+class Barco {
+public:
+    Barco();
+    int coordenadas[2][4]; // [x/y][indice]
+};
+
+Barco::Barco() {
+    cout << "Ingrese las coordenadas del barco (4 celdas)\n";
+    for (int i = 0; i < 4; i++) {
+        cout << "  Posicion " << i << ":\n";
+        cout << "   x: "; cin >> coordenadas[0][i];
+        cout << "   y: "; cin >> coordenadas[1][i];
+    }
+}
+
+
