406f6a0fa209f45f5bced800c037433118d53b88
JohanGaleano <johangaleano1139853@correo.itm.edu.co>
2025-09-27 21:12:37 -0500

    chore: repone Jugador.h mínimo y restituye shim ante archivos vacíos en feature/B-jugador

--- include/Jugador.h
+++ include/Jugador.h
@@ -1,12 +1,10 @@
-﻿#ifndef JUGADOR_H
-#define JUGADOR_H
-
-#include <string>
-
-class Jugador {
-public:
-    Jugador();
-    bool dispararAJugador(Jugador &enemigo, int fila, int col, int &idImpactado);
-};
-
+#ifndef JUGADOR_H  
+#define JUGADOR_H 
+ 
+class Jugador { 
+public: 
+    Jugador(); 
+    bool dispararAJugador(Jugador &enemigo, int fila, int col, int &idImpactado); 
+}; 
+ 
 #endif // JUGADOR_H
